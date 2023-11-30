df50ff12c36846c0905c90c8b9d604d241c6073f
Esra-Alhussain <esra.alhussain.s@gmail.com>
2023-11-30 09:31:49 -0800

    adding event handler to handle form submission

--- src/AddBookForm.js
+++ src/AddBookForm.js
@@ -1,2 +1,54 @@
 import React, { useState } from 'react';
 
+function AddBookForm(addBook){
+    // Step 4: Introduce state to manage user input
+    //This state will be used to store the user's input for a new book
+    //     1- Setting Up the State using useState hook
+    const [newBook, setNewBook] = useState('');
+
+    // Event handler to handle form submission
+    const handleSubmit = (e) => {
+        e.preventDefault(); //Prevents the default form submission behavior
+   
+    // Call the (AddBook) function passed from the parent as prop with the new book title
+    //to updating the parent's state with the new book title
+    addBook(newBook);
+
+    // Clear the input field after submitting
+    setNewBook('');
+    }
+
+    return (
+        //  Sets up the form with an onSubmit event listener that triggers the 
+        //handleSubmit function when the form is submitted.
+        // Associating the handleSubmit function with the form tag ensures that the form can be submitted not only by clicking the submit button
+        //  but also by pressing the Enter key when an input field is focused.
+        <form onSubmit ={handleSubmit}>
+            {/* Implementing a simple form */}
+            <label>
+            <input
+             type="text"
+             // ensures that the input field reflects the current state value (newBook
+             value={newBook}
+
+            //  onChange event listener is triggered every time the user types something in the input field.
+            //setNewBook = updates the newBook state with the new value entered by the user.
+             onChange={(e) => setNewBook(e.target.value)}
+             placeholder="Enter a new book"
+            />
+            </label>
+            <button>Add Book</button>
+        </form>
+    )
+}
+
+export default AddBookForm;
+
+
+// When the handleSubmit function is placed on the form tag:
+
+// Users can submit the form by clicking the submit button or pressing Enter while focusing on any input field within the form.
+// This is generally desirable for better accessibility and user experience.
+// When the handleSubmit function is placed on the button tag directly:
+
+// Users can submit the form by clicking the submit button, but pressing Enter might not trigger the form submission.
--- src/App.js
+++ src/App.js
@@ -1,27 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 // Importing React module
-import React from 'react';
+// useState hook is to make the list of books dynamic
+import React, { useState } from 'react';
+// import React from 'react';
 import BookList from './BookList';
 import AddBookForm from './AddBookForm'
 
+//useState is a React hook that allows you to add state to functional components.
 // Defining a functional component called App
 function App() {
-  const books = ["Book A", "Book B", "Book C"]; // Array of books
-  // JSX code for the component
+  // Step 4: Introduce state to manage the list of books
+  // initializes a state variable books with an initial array and a function setBooks to update the state.
+//   useState is a hook. It's a function provided by React that returns an array with two elements.
+// The first element (books in this case) is the current state value.
+// The second element (setBooks in this case) is a function that allows you to update the state.
+// [...] to set up an initial state when the component is first rendered.
+//setBooks is a function that you call with a new value to update the books state
+//by passing the new value as an argument 
+  const [books, setBooks] = useState([
+        "Book A",
+        "Book B",
+        "Book C"
+   ]); // The books state is initialized with an array of pre-defined books
+
+   // addBook function is to update the current state of books with a new book
+  const addBook = (newBook) => {
+    //update the books state by adding a new book to the existing array of books.
+    setBooks([...books, newBook]);
+  };
   return (
     <div className="App">
       <h1>My Favourite Books</h1>
       <p>Welcome to my book list page!</p>
       
       {/* Adding the BookList component */}
-      {/* Passing the list of books (Array) as props to BookList component  */}
+      {/* Display the BookList component & Passing the list of books (Array) as props to BookList component  */}
       <BookList books={books}/>
+      {/* AddBookForm component is to allow users to input new books */}
       <AddBookForm/>
     </div>
   );
 }
-
+// This will inform React of the state change, and React will then schedule a re-render of the component with the updated state. This ensures that React's virtual DOM is updated correctly and that your UI reflects the current state of your application.
 export default App;
 
 
